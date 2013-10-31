cfb86676cd6b473ed8126e2cdd1009f7d732bba8
Darren Hushak <dnhushak@pinnacle-recording.com>
2013-10-31 14:08:01 -0500

    ALU Control

--- Project-B/samples/cpu.vhd
+++ Project-B/samples/cpu.vhd
@@ -12,6 +12,8 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
 use work.mips32.all;
+use work.utils.all;
+use IEEE.numeric_std.all;
 
 entity cpu is
 	port(imem_addr  : out m32_word;     -- Instruction memory address
@@ -46,12 +48,14 @@ architecture structure of cpu is
 	end component;
 
 	-- 2-to-1 MUX
-	component mux2to1 is
-		generic(M : integer := 1);      -- Number of bits in the inputs and output
-		port(input0 : in  m32_vector(M - 1 downto 0);
-			 input1 : in  m32_vector(M - 1 downto 0);
-			 sel    : in  m32_1bit;
-			 output : out m32_vector(M - 1 downto 0));
+	component mux_Nbit_Min is
+		generic(N : integer := 32;
+			    M : integer := 2;
+			    A : integer := 1);      -- Number of bits in the inputs and output
+		port (i_C : in  std_logic_vector(A - 1 downto 0);
+			 --When declaring, M is the input port, N is the bit of that input port
+			 i_X       : in  array_Nbit(M - 1 downto 0, N - 1 downto 0);
+			 o_M       : out std_logic_vector(N - 1 downto 0));
 	end component;
 
 	--
